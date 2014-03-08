492600b31892b936b014afaf05da12515c2d03f4
KHReenberg <khreenberg@gmail.com>
2014-03-08 13:34:38 +0100

    Adding GUI before sharing first build

---
+++ Assets/Scripts/GameGUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class GameGUI : MonoBehaviour {
+
+    public float
+        NativeWidth = 960.0f,
+        NativeHeight = 540.0f;
+
+    protected Vector3 matrixVector;
+
+    void Awake() {
+        matrixVector = new Vector3( Screen.width / NativeWidth, Screen.height / NativeHeight );
+    }
+
+    void OnGUI() {
+        GUI.matrix = Matrix4x4.TRS( Vector3.zero, Quaternion.identity, matrixVector );
+    }
+}
