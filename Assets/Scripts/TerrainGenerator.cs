81fe9dacc36a208f6542c6faf5299191fd86217b
KHReenberg <khreenberg@gmail.com>
2014-03-08 10:27:49 +0100

    Random terrain is being generated. Maybe use perlin noise instead.

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+
+    public TerrainSegment TerrainSegmentPrefab;
+    private float width, speed;
+
+    void Awake() {
+        TerrainSegmentPrefab.CreatePool();
+        width = TerrainSegmentPrefab.Width;
+        width = width - ( width * TerrainSegmentPrefab.OverlapPercentage ); // Apply overlap to reduce jitter
+        speed = TerrainSegmentPrefab.MoveSpeed;
+        KickstartTerrainPool(30); // Spawn and recycle, in order to give the pool time to initialize before real spawning
+    }
+
+    private void Start() {
+        InvokeRepeating( "SpawnSegment", 0.3f, width / speed ); // "0.1f" can't be "0.0f", or it would spawn like three segments on first invoke. :/
+    }
+
+    private TerrainSegment SpawnSegment() {
+        Vector2 pos = transform.position;
+        pos.y += Random.Range( -1.3f, 1.3f ); // TODO: Make better terrain algorithm
+        return ObjectPool.Spawn( TerrainSegmentPrefab, pos );
+    }
+
+    private void KickstartTerrainPool( int count ) {
+        TerrainSegment[] b = new TerrainSegment[count];
+        for( int i = 0; i < count; i++ ) b[i] = SpawnSegment();
+        foreach( TerrainSegment segment in b ) segment.Recycle();
+    }
+}
---
+++ Assets/Scripts/TerrainSegment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class TerrainSegment : MonoBehaviour {
+
+    public float Width{ get { return transform.localScale.x; }}
+    public float OverlapPercentage = 0.01f;
+    public float MoveSpeed = 1.0f; // Would make it a const, but I also want to be able to tweak it in the inspector. :/
+
+    void Update() {
+        float vx = -MoveSpeed * Time.deltaTime;
+        transform.Translate(new Vector2(vx, 0));
+    }
+}
