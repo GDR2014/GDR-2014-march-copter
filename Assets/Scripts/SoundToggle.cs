2d1c48549092b85d38f11e3024b56fc304cb71ef
KHReenberg <khreenberg@gmail.com>
2014-03-10 20:14:54 +0100

    Adds a mute button. Functionality is sporadic

---
+++ Assets/Scripts/SoundToggle.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SoundToggle : MonoBehaviour {
+
+    private bool _muted;
+    public bool IsMuted {
+        get {
+            return _muted;
+        }
+        set {
+            _muted = value;
+            PlayerPrefs.SetInt("muted", _muted ? 0 : 1);
+        }
+    }
+    public Sprite OnSprite, OffSprite;
+    AudioSource[] AudioSources;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake() {
+        IsMuted = PlayerPrefs.GetInt( "muted", 0 ) > 0;
+        AudioSources = FindObjectsOfType<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Start() {
+        SetSprite();
+        SetVolume();
+    }
+
+    void ToggleSound() {
+        IsMuted = !IsMuted;
+        SetVolume();
+        SetSprite();
+    }
+
+    void SetVolume() {
+        foreach( var source in AudioSources )
+            source.volume = IsMuted ? 0.0f : 1.0f;
+    }
+
+    void SetSprite() {
+        spriteRenderer.sprite = IsMuted ? OffSprite : OnSprite;
+    }
+
+    void OnMouseDown() {
+        ToggleSound();
+    }
+}
